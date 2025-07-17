95bfff52cb17b0b8260ead6f2f9d6f17ee7d1d55
Ryan Schmidt <rms@rms80.com>
2025-07-16 23:50:35 -0400

    add commandline argument query/parsing nodefuncs

---
+++ GSNodeGraphCoreLibrary/EnvironmentNodes.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradientspace.NodeGraph.Nodes
+{
+	[GraphNodeFunctionLibrary("Gradientspace.Environment")]
+	public static class GradientspaceEnvironmentFunctionLibrary
+	{
+		//! run environments that support arguments (eg command-line, etc) should set them
+		//! here and then the argument nodes below will fetch them
+		public static string[] GlobalArguments = new string[] { };
+
+
+		[NodeFunction]
+		public static void SetArgs(string argumentString)
+		{
+			string[] args = argumentString.Split();
+			GlobalArguments = args;
+		}
+
+
+		[NodeFunction]
+		public static int GetNumArgs()
+		{
+			return GlobalArguments.Length;
+		}
+
+		[NodeFunction]
+		public static string GetArg(int Index)
+		{
+			if (Index >= 0 && Index < GlobalArguments.Length)
+				return GlobalArguments[Index];
+			return "";
+		}
+
+		[NodeFunction]
+		public static List<string> GetAllArgs()
+		{
+			return GlobalArguments.ToList();
+		}
+
+		//! search for argument that begins with Prefix, and return string with Prefix stripped off
+		[NodeFunction]
+		public static string FindArgFromPrefix(string Prefix, string DefaultString = "")
+		{
+			string? found = find_argument_from_prefix(Prefix, true);
+			return found ?? DefaultString;
+		}
+
+		[NodeFunction]
+		public static int FindIntArgFromPrefix(string Prefix, int DefaultValue = 0)
+		{
+			string? found = find_argument_from_prefix(Prefix, true);
+			if (int.TryParse(found, out int Value))
+				return Value;
+			return DefaultValue;
+		}
+
+		[NodeFunction]
+		public static double FindRealArgFromPrefix(string Prefix, double DefaultValue = 0)
+		{
+			string? found = find_argument_from_prefix(Prefix, true);
+			if (double.TryParse(found, out double Value))
+				return Value;
+			return DefaultValue;
+		}
+
+		[NodeFunction]
+		public static bool FindBoolArgFromPrefix(string Prefix, bool bDefaultValue = false)
+		{
+			string? found = find_argument_from_prefix(Prefix, true);
+			if (bool.TryParse(found, out bool Value))
+				return Value;
+			return bDefaultValue;
+		}
+
+
+
+
+		// internals/utilities
+
+		public static string? find_argument_from_prefix(string prefix, bool bIgnoreCase)
+		{
+			StringComparison useComparison = (bIgnoreCase) ? StringComparison.InvariantCultureIgnoreCase : StringComparison.InvariantCulture;
+
+			for ( int i = 0; i < GlobalArguments.Length; i++ )
+			{
+				if (GlobalArguments[i].StartsWith(prefix, useComparison))
+					return GlobalArguments[i].Substring(prefix.Length);
+			}
+			return null;
+		}
+
+
+	}
+
+
+	internal class EnvironmentNodes
+	{
+	}
+}
