b21ac408aa20ba579e96bee03d0f5153c8bd26ee
Ryan Schmidt <rms@rms80.com>
2025-12-08 17:41:35 -0500

    add support for variables in nodelibrary paths

--- GSNodeGraph/Util/NodeLibraryUtils.cs
+++ GSNodeGraph/Util/NodeLibraryUtils.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Diagnostics;
 
 namespace Gradientspace.NodeGraph.Util
 {
     public static class NodeLibraryUtils
     {
+        public const string INSTALL_FOLDER = "%INSTALLDIR%";
+
         public static int FindAndLoadNodeLibraries(string directoryPath, bool bIncludeSubFolders = true)
         {
             int NumLoaded = 0;
@@ -37,12 +40,25 @@ namespace Gradientspace.NodeGraph.Util
 
         public static int FindAndLoadNodeLibraries(IEnumerable<string> paths, bool bIncludeSubFolders = true)
         {
+            // installer actually creates a registry key on windows...
+            string appPath = AppDomain.CurrentDomain.BaseDirectory;
+            if ( Debugger.IsAttached || appPath.Contains("net9.0") )
+                appPath = Path.Combine(appPath, "..", "..", "..", "..");        // this is hardcoded for rms80 dev environment, for now... (how to do another way??)
+            else
+                appPath = Path.Combine(appPath, "..");                          // in installed build, exe is copied in subfolder of installer folder
+            appPath = Path.GetFullPath(appPath);
+
             int NumLoaded = 0;
             foreach (string path in paths)
             {
-                if ( Directory.Exists(path) )
+                // handle app-specific variables in paths
+                string fullPath = path;
+                if (path.Contains(INSTALL_FOLDER))
+                    fullPath = path.Replace(INSTALL_FOLDER, appPath);
+
+                if ( Directory.Exists(fullPath) )
                 {
-                    NumLoaded += FindAndLoadNodeLibraries(path, bIncludeSubFolders);
+                    NumLoaded += FindAndLoadNodeLibraries(fullPath, bIncludeSubFolders);
                 }
             }
             return NumLoaded;
