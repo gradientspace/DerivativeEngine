b9de5eb3d15f9a535c543f8d0944660aa9ce9653
Ryan Schmidt <rms@rms80.com>
2025-12-21 22:13:01 -0500

    add ObjectViewNode
    Takes object input and shows type + ToString() info, via same NodeWidget as StringViewNode. Has a dynamic output pin, so that wire-through can be done cleanly.

--- GSNodeGraphCoreLibrary/PrintNodes.cs
+++ GSNodeGraphCoreLibrary/PrintNodes.cs
@@ -1,6 +1,7 @@
 // Copyright Gradientspace Corp. All Rights Reserved.
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -123,6 +124,78 @@ namespace Gradientspace.NodeGraph.Nodes
 
     }
 
+
+
+
+    /**
+     * Object View Node - allows viewing any object as a string in the UI
+     * This is DynamicOutput node - the output type will be updated to match
+     * the type of object wired into the input.
+     * (currently the input type does not change, it is always 'object')
+     */
+    [GraphNodeNamespace("Core.IO")]
+    public class ObjectViewNode : NodeBase, INode_DynamicOutputs
+    {
+        public override string GetDefaultNodeName() { return "ViewObject"; }
+
+        public const string ObjectInputName = "object";
+        public const string ObjectOutputName = "object";
+
+        StandardNodeInputBase ObjInput;
+        StandardNodeOutputBase ObjOutput;
+
+        public ObjectViewNode()
+        {
+            ObjInput = new StandardNodeInputBase(typeof(object));
+            AddInput(ObjectInputName, ObjInput);
+            ObjInput.Flags |= ENodeInputFlags.IsInOut;
+            ObjInput.Flags |= ENodeInputFlags.HiddenLabel;
+
+            ObjOutput = new StandardNodeOutputBase(typeof(object));
+            AddOutput(ObjectOutputName, ObjOutput);
+            ObjOutput.Flags |= ENodeOutputFlags.HiddenLabel;
+        }
+
+        public delegate void StringViewUpdateEvent(string String);
+        public event StringViewUpdateEvent? OnStringUpdate;
+
+        public override void Evaluate(EvaluationContext EvalContext, ref readonly NamedDataMap DataIn, NamedDataMap RequestedDataOut)
+        {
+            int OutputIndex = RequestedDataOut.IndexOfItem(ObjectOutputName);
+            if (OutputIndex == -1)
+                throw new Exception($"{GetDefaultNodeName()}: output not found");
+
+            object? foundData = DataIn.FindItemValue(ObjectInputName);
+            string stringValue = "null";
+            if ( foundData != null ) {
+                stringValue = $"[{foundData.GetType().ToString()}]\n{foundData.ToString()}";
+            }
+            OnStringUpdate?.Invoke(stringValue);
+            RequestedDataOut.SetItem(OutputIndex, ObjectOutputName, foundData!);
+        }
+
+        public void UpdateDynamicOutputs(INodeGraph graph)
+        {
+            IConnectionInfo connection = graph.FindConnectionTo(this.GraphIdentifier, ObjectInputName, EConnectionType.Data);
+            if (connection.IsValid == false)
+                return;
+
+            INodeInfo fromNodeInfo = graph.FindNodeFromIdentifier(connection.FromNodeIdentifier);
+            bool bFoundType = graph.GetNodeOutputType(connection.FromNodeIdentifier, connection.FromNodeOutputName, out GraphDataType incomingDataType);
+            if (bFoundType == false)
+                return;
+
+            if (ObjOutput.GetDataType().IsSameType(incomingDataType) == false) {
+                Outputs.Clear();
+                ObjOutput = new StandardNodeOutputBase(incomingDataType.CSType);
+                ObjOutput.Flags |= ENodeOutputFlags.HiddenLabel;
+                AddOutput(ObjectOutputName, ObjOutput);
+
+                // publish node change, this will rebuild it at the UI level
+                PublishNodeModifiedNotification();
+            }
+        }
+    }
 
 
 }
